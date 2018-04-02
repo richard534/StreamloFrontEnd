b72598ea9b40f571229795b561c510091e43d808
Richard Taylor <richard534@hotmail.co.uk>
2018-04-02 22:19:39 +0100

    [REFAC] Added bundle optimizations

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,16 +3,9 @@ const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
 
 const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
+const CompressionPlugin = require("compression-webpack-plugin");
 
 module.exports = merge(common, {
-  mode: "production",
   devtool: "source-map",
-  plugins: [
-    new UglifyJSPlugin({
-      sourceMap: true
-    }),
-    new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify("production")
-    })
-  ]
+  plugins: [new CompressionPlugin()]
 });
