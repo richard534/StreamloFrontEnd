051c0d3fef695973902ef561b34431a3dbb5f788
Richard Taylor <richard534@hotmail.co.uk>
2018-04-22 13:49:30 +0100

    [REFAC] Added bundle analysis npm script

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import React from "react";
 import { render } from "react-dom";
 import { Router, browserHistory } from "react-router";
 import routes from "./routes";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import "./css/custom.css"; // Importing custom css after bootstrap is important!
 import "../node_modules/toastr/toastr.css";
 
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,7 +2,6 @@ const webpack = require("webpack");
 const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
 
-const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 const CompressionPlugin = require("compression-webpack-plugin");
 
 module.exports = merge(common, {
